53fed39b106fc4f23f642a1b9b60fae2176a8654
Brightproject <brightproject@gmail.com>
2013-05-14 14:08:09 +0300

    add info

--- views/bankaddView.php
+++ views/bankaddView.php
@@ -15,18 +15,43 @@
 
 <form name="form" method="post" action="">
         <p align="center">
-          <label>Логин<br /><br />
-            <input value="" type="text" name="login" id="login">
+          <label>Имя банка<br /><br />
+            <input value="" type="text" name="bank" id="bank">
             </label>
           </p>
         <p align="center">
-          <label>Пароль<br /><br />
-          <input value="" type="text" name="pass" id="pass">
+          <label>Досрочное погашение<br /><br />
+          <input value="" type="text" name="early_repay" id="early_repay">
           </label>
         </p>
         <p align="center">
-          <label>Электронка<br /><br />
+          <label>Ставка в рублях<br /><br />
           <input value="" type="text" name="email" id="email">
+          </label>
+        </p>
+        <p align="center">
+          <label>Первоначальный платеж<br /><br />
+          <input value="" type="text" name="first_pay" id="first_pay">
+          </label>
+        </p>
+        <p align="center">
+          <label>Срок кредита<br /><br />
+          <input value="" type="text" name="credit_period" id="credit_period">
+          </label>
+        </p>
+        <p align="center">
+          <label>Срок рассмотрения<br /><br />
+          <input value="" type="text" name="period_of_review" id="period_of_review">
+          </label>
+        </p>
+        <p align="center">
+          <label>Дополнительные расходы<br /><br />
+          <input value="" type="text" name="add_costs" id="add_costs">
+          </label>
+        </p>		
+        <p align="center">
+          <label>Документы<br /><br />
+          <input value="" type="text" name="docs" id="docs">
           </label>
         </p>
         <p align="center">
